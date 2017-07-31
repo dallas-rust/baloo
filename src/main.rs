1c256b9623907d19384c5d660316d4517ab55351
J/A <archer884@gmail.com>
2017-07-31 10:50:03 -0500

    Add sample telemetry endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+#[macro_use]
+extern crate serde_derive;
+
 extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::Json;
 
 fn main() {
     rocket::ignite().mount("/", routes![hello]).launch();
@@ -10,4 +16,24 @@ fn main() {
 #[get("/")]
 fn hello() -> &'static str {
     "Hi"
+}
+
+// Here we have a sample of a telemetry route. It's not at all complete, of course...
+//
+// The first problem here is the telemetry struct itself: we need to canonicalize heading into 
+// a single kind of measurement, and of course that measurement probably needs to be in degrees 
+// or radians as an f32 or f64 or something along those lines--you know, rather than a String.
+//
+// In addition to transforming this json telemetry packet into usable data, we need to store that 
+// data as part of the "airplane's" state.
+
+#[derive(Deserialize)]
+struct Heading {
+    heading: String,
+    measurement: String,
+}
+
+#[post("/telemetry/heading", data = "<heading>")]
+fn tel_heading(heading: Json<Heading>) -> &'static str {
+    "Ok!"
 }
